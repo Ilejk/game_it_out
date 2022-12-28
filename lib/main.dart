9571f239dda554129755b2fdaf1089e3b06f54fa
Ilejk <adrianskibaa@gmail.com>
2022-12-28 22:12:52 +0100

    28.12.2022 22:12

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_logout_simple_ui/auth_page.dart';
+import 'package:login_logout_simple_ui/pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
