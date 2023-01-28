3e242e11f1304f3e45e7e381e7f9b4ae194b882d
Ilejk <adrianskibaa@gmail.com>
2023-01-28 13:23:35 +0100

    task widget changes

--- lib/widgets/task_widget.dart
+++ lib/widgets/task_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:login_logout_simple_ui/widgets/shadow_box_container.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:login_logout_simple_ui/constants/color_constants.dart';
 import '../constants/icons_constants.dart';
 import '../constants/padding_constants.dart';
 import '../constants/sizes_constants.dart';
@@ -10,71 +11,96 @@ class TaskWidget extends StatelessWidget {
   final String taskName;
   final String difficulty;
   final double taskLenght;
-  final Function()? onTap;
   final double expGained;
+  final Function(BuildContext)? deleteTask;
+  final Function(BuildContext)? taskFinished;
 
   const TaskWidget({
     super.key,
     required this.taskName,
     required this.difficulty,
     required this.taskLenght,
-    required this.onTap,
     required this.expGained,
+    required this.deleteTask,
+    required this.taskFinished,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: PaddingConstants.kTaskPadding,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: PaddingConstants.kBasePadding10,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  taskName,
-                  textAlign: TextAlign.left,
-                  style: TextStyleConstants.kTaskTitleTextStyle,
-                ),
-                SizesConstants.kSizedBox15height,
-                Text(
-                  difficulty,
-                  textAlign: TextAlign.left,
-                  style: TextStyleConstants.kTaskSubTitleTextStyle,
-                ),
-                SizesConstants.kSizedBox15height,
-                Text(
-                  '${StringConstants.kDuration}  $taskLenght ${StringConstants.kHours}',
-                  textAlign: TextAlign.left,
-                  style: TextStyleConstants.kTaskSubTitleTextStyle,
-                ),
-              ],
+      child: Slidable(
+        startActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              borderRadius: SizesConstants.kBorderRadius12,
+              onPressed: taskFinished,
+              icon: IconsConstants.kTaskFinished,
+              backgroundColor: ColorConstatns.kGreen,
             ),
+          ],
+        ),
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              borderRadius: SizesConstants.kBorderRadius12,
+              onPressed: deleteTask,
+              icon: IconsConstants.kDelete,
+              backgroundColor: ColorConstatns.kRed,
+            ),
+          ],
+        ),
+        child: SizedBox(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: PaddingConstants.kBasePadding10,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 250,
+                      child: Text(
+                        taskName,
+                        textAlign: TextAlign.left,
+                        style: TextStyleConstants.kTaskTitleTextStyle,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        softWrap: false,
+                      ),
+                    ),
+                    SizesConstants.kSizedBox15height,
+                    Text(
+                      difficulty,
+                      textAlign: TextAlign.left,
+                      style: TextStyleConstants.kTaskSubTitleTextStyle,
+                    ),
+                    SizesConstants.kSizedBox15height,
+                    Text(
+                      '${StringConstants.kDuration}  $taskLenght ${StringConstants.kHours}',
+                      textAlign: TextAlign.left,
+                      style: TextStyleConstants.kTaskSubTitleTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: PaddingConstants.kBaseTaskTilePadding,
+                child: Column(
+                  children: [
+                    Text(
+                      '$expGained  ${StringConstants.kExp}',
+                      style: TextStyleConstants.kTaskSubTitleTextStyle,
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          Padding(
-            padding: PaddingConstants.kBasePadding10,
-            child: Column(
-              children: [
-                Text(
-                  '$expGained  \n${StringConstants.kExp}',
-                  style: TextStyleConstants.kTaskSubTitleTextStyle,
-                ),
-                SizesConstants.kSizedBox15height,
-                ShadowBoxContainer(
-                  height: SizesConstants.kTaskDoneIconBoxHeight,
-                  width: SizesConstants.kTaskDoneIconBoxWidth,
-                  child: IconButton(
-                    onPressed: onTap,
-                    icon: IconsConstants.kDoneTaskIcon,
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
